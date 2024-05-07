2211a287382f812cc50dcba3cef4835b311350d0
Artur <artur.balsis@gmail.com>
2024-05-07 16:50:31 +0300

    add comment for pytest.mark.parametrize

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -4,7 +4,7 @@ import pytest
 
 from pages.product_page import ProductPage
 
-
+# list(range(1, 7)) + [pytest.param(7, marks=pytest.mark.xfail)] + list(range(8, 10))
 @pytest.mark.parametrize('num', [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail), *range(8, 10)])
 def test_guest_can_add_product_to_basket(driver, num):
     URL = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
